08e0edb4d43587a8db0652d97e90e30c67b9a20a
boogie666 <bbugarschi@gmail.com>
2017-08-06 23:12:06 +0300

    ql html parser done

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject quick-lunch-parser-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [enlive "1.1.6"]
+                 [http-kit "2.1.18"]])
